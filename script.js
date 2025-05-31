ffcbba7cbe7c20d9a541cdec672d8865692f5f60
Atul Pal <66594774+atsin6@users.noreply.github.com>
2025-06-01 00:03:18 +0530

    New Design, Checkbox, Rendring Logic

--- script.js
+++ script.js
@@ -1,85 +1,131 @@
-let no_of_todos = 4;
+const inputEle = document.querySelector("#todo-input");
+inputEle.classList.add("input-ele");
 
-// ------------------- Add Todo button -------------------
+let todos = [
+  {
+    title: "Make Todo list project.",
+    checkbox: true,
+  },
+];
+
+render();
+let no_of_todos = 1;
+
 function addTodo() {
-  no_of_todos++;
-  const inputEl = document.querySelector("#add-todo-input");
-  const value = inputEl.value;
+  const value = inputEle.value.trim();
+  if (value === "") return;
 
-  let newEntry = document.createElement("div");
-  newEntry.classList.add("todo-container-" + no_of_todos, "todo-div");
+  todos.push({
+    todo_no: ++no_of_todos,
+    title: value,
+    checkbox: false,
+  });
+  render();
 
-  let todoDetails = document.createElement("h4");
-  todoDetails.classList.add("todo", "todo-" + no_of_todos);
-  todoDetails.innerHTML = no_of_todos + ". " + value;
-
-  let btnsDiv = document.createElement("div");
-  btnsDiv.classList.add("btns-div");
-
-  let deleteBtn = document.createElement("button");
-  deleteBtn.setAttribute("id", "btn");
-  deleteBtn.innerHTML = "delete";
-  deleteBtn.setAttribute("onclick", "deleteTodo(" + no_of_todos + ")");
-
-  let editBtn = document.createElement("button");
-  editBtn.setAttribute("id", "btn");
-  editBtn.innerHTML = "edit";
-  editBtn.setAttribute("onclick", "editTodo(" + no_of_todos + ")");
-
-  document.querySelector("#todos").appendChild(newEntry);
-  newEntry.appendChild(todoDetails);
-  newEntry.appendChild(btnsDiv);
-
-  btnsDiv.appendChild(editBtn);
-  btnsDiv.appendChild(deleteBtn);
-
-  inputEl.value = "";
+  inputEle.value = "";
 }
 
-// ------------------- Delete Todo button -------------------
-function deleteTodo(index) {
-  let element = document.querySelector(".todo-container-" + index);
-  element.parentNode.removeChild(element);
+function deleteTodo(idx) {
+  todos.splice(idx, 1);
+  render();
 }
 
-// ------------------- Edit Todo button -------------------
 function editTodo(index) {
   let editedInput = document.createElement("input");
-  let todo_str = ".todo-" + index;
-  let todo = document.querySelector(todo_str);
-  editedInput.value = todo.innerHTML.slice(3);
+  editedInput.classList.add("input-ele");
+  editedInput.value = todos[index].title;
 
-  let element = document.querySelector(".todo-container-" + index);
-  element.replaceChild(editedInput, todo);
+  let titleEL = document.querySelector("#todo-span-" + index);
+  console.log(titleEL);
+
+  titleEL.parentNode.replaceChild(editedInput, titleEL);
 
   editedInput.focus();
-
   editedInput.addEventListener("keydown", (event) => {
-    let key = event.key;
-    console.log(`Key pressed: ${key}`);
     if (event.key === "Enter") {
-      let newInput = editedInput.value;
-
-      let savingNewInput = document.createElement("h4");
-      savingNewInput.classList.add("todo", "todo-" + index);
-      savingNewInput.innerHTML = index + ". " + newInput;
-
-      editedInput.replaceWith(savingNewInput);
+      todos[index].title = editedInput.value;
+      render();
     }
   });
 }
 
-// ------------------- Clock -------------------
+function render() {
+  //   console.log("rendering");
 
-// function clock_running() {
-//   let date = new Date();
-//   let hr = date.getHours();
-//   let min = date.getMinutes();
-//   let sec = date.getSeconds();
+  let todosEL = document.querySelector("#todos");
+  todosEL.replaceChildren();
 
-//   let time = hr + ":" + min + ":" + sec;
-//   let clock = document.querySelector("#time");
-//   clock.innerHTML = time;
-// }
+  for (let i = 0; i < todos.length; i++) {
+    todosEL.appendChild(createTodoComponenet(i));
+  }
+}
 
-// setInterval(clock_running, 1000);
+function createTodoComponenet(i) {
+  let todo = todos[i];
+
+  let todoDivEl = document.createElement("div");
+  todoDivEl.setAttribute("id", "todo-div-" + i);
+  todoDivEl.setAttribute("class", "todo-div");
+
+  //---------------
+  let labelEl = document.createElement("label");
+  let inputEl = document.createElement("input");
+  let title_spanEl = document.createElement("span");
+  let btn_divEl = document.createElement("div");
+  let editBtnEl = document.createElement("button");
+  let editImg = document.createElement("img");
+  let deleteBtnEl = document.createElement("button");
+  let deleteImg = document.createElement("img");
+
+  labelEl.setAttribute("for", "todo-" + i);
+  labelEl.setAttribute("id", "todo-label-" + i);
+  inputEl.setAttribute("type", "checkbox");
+  inputEl.setAttribute("id", "todo-" + i);
+
+  inputEl.addEventListener("change", (event) => {
+    let idx = event.currentTarget.id;
+    idx = idx.split("-")[1];
+    if (inputEl.checked) {
+      todos[idx].checkbox = true;
+      render();
+    } else if (!inputEl.checked) {
+      todos[idx].checkbox = false;
+      render();
+    }
+  });
+
+  title_spanEl.innerHTML = i + 1 + ". " + todo.title;
+  title_spanEl.setAttribute("id", "todo-span-" + i);
+
+  if (todo.checkbox == true) {
+    inputEl.checked = true;
+    title_spanEl.classList.add("lineThrough");
+  } else {
+    inputEl.checked = false;
+    title_spanEl.classList.remove("lineThrough");
+  }
+  title_spanEl.innerHTML = i + 1 + ". " + todo.title;
+
+  btn_divEl.setAttribute("id", "btn-div");
+
+  editImg.setAttribute("src", "pen.png");
+  editImg.classList.add("btn-img");
+
+  deleteImg.setAttribute("src", "x-mark.png");
+  deleteImg.classList.add("btn-img");
+  deleteImg.setAttribute("alt", "Delete");
+
+  editBtnEl.setAttribute("onclick", "editTodo(" + i + ")");
+  editBtnEl.appendChild(editImg);
+  deleteBtnEl.setAttribute("onclick", "deleteTodo(" + i + ")");
+  deleteBtnEl.appendChild(deleteImg);
+
+  labelEl.appendChild(inputEl);
+  labelEl.appendChild(title_spanEl);
+  btn_divEl.appendChild(editBtnEl);
+  btn_divEl.appendChild(deleteBtnEl);
+  todoDivEl.appendChild(labelEl);
+  todoDivEl.appendChild(btn_divEl);
+
+  return todoDivEl;
+}
